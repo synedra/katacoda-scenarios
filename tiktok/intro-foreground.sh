865d75271a3b058aed24f7a7ae12438a69c3f02a
synedra-datastax <77410784+synedra-datastax@users.noreply.github.com>
2021-04-14 10:46:39 -0700

    Adding repo/install/setup to step 2

--- tiktok/intro-foreground.sh
+++ tiktok/intro-foreground.sh
@@ -1,4 +1,7 @@
 echo "Getting the tiktok repository to work with"
-git clone https://github.com/synedra/tik-tok.git .
+git clone https://github.com/synedra/tik-tok.git
+cd tik-tok
 npm install
 node setup.js
+cd ..
+echo "Done getting the tiktok repo for working with"
